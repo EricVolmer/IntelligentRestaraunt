422f319b4f9a9d0d55be67c23d63940fa2db838f
Eric Volmer <37138635+EricVolmer@users.noreply.github.com>
2021-01-05 23:27:55 +0100

    Old code taken over, small fixes done. Waiting to be converted to Kendo UI MVC

--- IntelligentRestaraunt/Controllers/HomeController.cs
+++ IntelligentRestaraunt/Controllers/HomeController.cs
@@ -1,28 +1,206 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using IntelligentRestaraunt.Models;
 
 namespace IntelligentRestaraunt.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly string baseUrl = "http://localhost:8080/api/";
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Food()
         {
-            ViewBag.Message = "Your application description page.";
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Drinks()
         {
-            ViewBag.Message = "Your contact page.";
             return View();
+        }
+
+        public ActionResult Meat()
+        {
+            IEnumerable<productViewModel> productViewModels = new List<productViewModel>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var responseTask = client.GetAsync("productViewModel/byType/meat");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync().Result;
+                    productViewModels = JsonConvert.DeserializeObject<IEnumerable<productViewModel>>(readTask);
+                }
+
+                return View(productViewModels);
+            }
+        }
+
+        public ActionResult Soup()
+        {
+            IEnumerable<productViewModel> productViewModels = new List<productViewModel>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var responseTask = client.GetAsync("productViewModel/byType/soup");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync().Result;
+                    productViewModels = JsonConvert.DeserializeObject<IEnumerable<productViewModel>>(readTask);
+                }
+
+                return View(productViewModels);
+            }
+        }
+
+        public ActionResult Snacks()
+        {
+            IEnumerable<productViewModel> productViewModels = new List<productViewModel>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var responseTask = client.GetAsync("productViewModel/byType/snacks");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync().Result;
+                    productViewModels = JsonConvert.DeserializeObject<IEnumerable<productViewModel>>(readTask);
+                }
+
+                return View(productViewModels);
+            }
+        }
+
+        public ActionResult Dessert()
+        {
+            IEnumerable<productViewModel> productViewModels = new List<productViewModel>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var responseTask = client.GetAsync("productViewModel/byType/dessert");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync().Result;
+                    productViewModels = JsonConvert.DeserializeObject<IEnumerable<productViewModel>>(readTask);
+                }
+
+                return View(productViewModels);
+            }
+        }
+
+        public ActionResult SoftDrink()
+        {
+            IEnumerable<productViewModel> productViewModels = new List<productViewModel>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var responseTask = client.GetAsync("productViewModel/byType/sdrink");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync().Result;
+                    productViewModels = JsonConvert.DeserializeObject<IEnumerable<productViewModel>>(readTask);
+                }
+
+                return View(productViewModels);
+            }
+        }
+
+        public ActionResult StrongDrink()
+        {
+            IEnumerable<productViewModel> productViewModels = new List<productViewModel>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var responseTask = client.GetAsync("productViewModel/byType/stdrink");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync().Result;
+                    productViewModels = JsonConvert.DeserializeObject<IEnumerable<productViewModel>>(readTask);
+                }
+
+                return View(productViewModels);
+            }
+        }
+
+        public ActionResult GetproductViewModels()
+        {
+            IEnumerable<productViewModel> productViewModels = new List<productViewModel>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var responseTask = client.GetAsync("productViewModel/byType/snacks");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync().Result;
+                    productViewModels = JsonConvert.DeserializeObject<IEnumerable<productViewModel>>(readTask);
+                }
+
+                return View(productViewModels);
+            }
+        }
+
+        public ActionResult getHelp()
+        {
+            var tableNo = new productViewModelOrder().tableNO;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var responseTask = client.GetAsync("help/needHelp/" + tableNo);
+                responseTask.Wait();
+            }
+
+            Console.WriteLine("here" + tableNo);
+
+            return RedirectToAction("Index");
         }
     }
 }
--- IntelligentRestaraunt/Models/productViewModel.cs
+++ IntelligentRestaraunt/Models/productViewModel.cs
@@ -8,7 +8,7 @@ namespace IntelligentRestaraunt.Models
     {
         public int productID { get; set; }
         public string productName { get; set; }
+        public string productDescription { get; set; }
         public int productPrice { get; set; }
-        
     }
 }
